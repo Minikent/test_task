9243f620f99092e8bc31e33371dc7111f4991c59
Dmitry <minikent5@yandex.ru>
2018-05-17 18:18:19 +0300

    comment about promise

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var ArrayCompressor = require('./components/ArrayCompressor');
+
 function arrayCompress(arr, callback) {
+  // Не использовал Promise и async/await, знаю о них, но нужно повторить тему
   setImmediate(function() {
     var compressor = new ArrayCompressor(arr);
     callback(compressor.process());
